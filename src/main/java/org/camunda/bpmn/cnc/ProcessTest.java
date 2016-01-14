633603985a4ca618d0aba32f6593af306644b6f7
David Koch <david.koch.1992@gmail.com>
2016-01-14 11:22:57 +0100

    New java project

--- src/main/java/org/camunda/bpmn/cnc/ProcessTest.java
+++ src/main/java/org/camunda/bpmn/cnc/ProcessTest.java
@@ -3,7 +3,7 @@ package org.camunda.bpmn.cnc;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
 
-@ProcessApplication("Cars and Co")
+@ProcessApplication
 public class ProcessTest extends ServletProcessApplication{
 
 }
