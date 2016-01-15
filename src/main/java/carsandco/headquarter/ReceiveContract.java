99f5b7dae11d0316082015bed5174fca404bb00f
David <davidkoch@uni-koblenz.de>
2016-01-15 10:43:40 +0100

    Added JSON Dependency and Process Start by Message receive

---
+++ src/main/java/carsandco/headquarter/ReceiveContract.java
@@ -0,0 +1,51 @@
+package carsandco.headquarter;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class ReceiveContract {
+
+		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+
+		@POST
+		@Path("/newcontract")
+		@Consumes(MediaType.APPLICATION_JSON)
+		public Response crunchifyREST(InputStream incomingData) {
+			StringBuilder crunchifyBuilder = new StringBuilder();
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(incomingData));
+				String line = null;
+				while ((line = in.readLine()) != null) {
+					crunchifyBuilder.append(line);
+				}
+				Map<String,Object> map = new HashMap<String,Object>();
+				map.put("contract", crunchifyBuilder.toString());
+				ProcessInstance processInstance = runtimeService.startProcessInstanceByMessage("contract", map);
+				
+				System.out.println("Data Received: " + crunchifyBuilder.toString());
+				System.out.println("Process ID: " + processInstance.getId());
+				
+			} catch (Exception e) {
+				System.out.println("Error Parsing: - ");
+			}
+			
+			// return HTTP response 200 in case of success
+			return Response.status(200).entity(crunchifyBuilder.toString()).build();
+		}
+}
